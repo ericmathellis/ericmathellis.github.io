8b383dc4fbcd2e3e097669cd4af618d519bc3670
Eric Ellis <ericramone@gmail.com>
2021-09-26 12:40:00 -0400

    tifs

--- dist/script.js
+++ dist/script.js
@@ -5,10 +5,10 @@ var baseUrl = '';
 
 var pano = new Spherical({
 	container: 'body',
-	front: baseUrl + '1.png',
-	back: baseUrl + '2.png',
-	left: baseUrl + '3.png',
-	right: baseUrl + '4.png',
-	top: baseUrl + '5.png',
-	bottom: baseUrl + '6.png'
+	front: baseUrl + 'front.tif',
+	back: baseUrl + 'back.tif',
+	left: baseUrl + 'left.tif',
+	right: baseUrl + 'right.tif',
+	top: baseUrl + 'top.tif',
+	bottom: baseUrl + 'bottom.tif'
 });
